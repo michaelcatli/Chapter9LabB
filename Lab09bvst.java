f6e6434b75dd3a7d4dcba69a6aefa81bbc110f39
Catli <10312@vtsd.com>
2019-12-16 08:14:10 -0500

    Done!

--- Lab09bvst.java
+++ Lab09bvst.java
@@ -23,6 +23,10 @@ public class Lab09bvst
 		System.out.println("\n\n" + r1.getOriginal() + " * " + r2.getOriginal() + "  =  " + r3.getReduced());
 		r3.divide(r1,r2);
 		System.out.println("\n" + r1.getOriginal() + " / " + r2.getOriginal() + "  =  " + r3.getReduced());
+		r3.add(r1,r2);
+		System.out.println("\n" + r1.getOriginal() + " + " + r2.getOriginal() + "  =  " + r3.getReduced());
+		r3.subtract(r1,r2);
+		System.out.println("\n" + r1.getOriginal() + " - " + r2.getOriginal() + "  =  " + r3.getReduced());
 
 //		100 Point Version Only
 //		r3.add(r1,r2);
@@ -53,9 +57,51 @@ class Rational
 	private int firstDen;	   // entered denominator
 	private int reducedNum;		// reduced numerator
 	private int reducedDen;		// reduced denominator
-   private int gcf;           // greatest common factor
+  private int gcf;           // greatest common factor
 
-	
+	public Rational(int n, int d){
+		firstNum=n;
+		firstDen=d;
+	}
+	public Rational(){
+		firstNum=1;
+		firstDen=1;
+	}
+	private void reduce(){
+		getGCF(firstNum, firstDen);
+		reducedNum = firstNum/gcf;
+		reducedDen = firstDen/gcf;
+	}
+	public String getReduced(){
+		String redfrac = "" + reducedNum + "/" + reducedDen;
+		return redfrac;
+	}
+	public String getOriginal(){
+		String frac = "" + firstNum + "/" + firstDen;
+		return frac;
+	}
+	public void multiply(Rational n1, Rational n2){
+		this.firstNum = n1.firstNum * n2.firstNum;
+		this.firstDen = n1.firstDen * n2.firstDen;
+		reduce();
+	}
+	public void divide(Rational n1, Rational n2){
+		this.firstNum = n1.firstNum * n2.firstDen;
+		this.firstDen = n1.firstDen * n2.firstNum;
+		reduce();
+	}
+	public void add(Rational n1, Rational n2){
+		int p = n1.firstDen*n2.firstDen;
+		this.firstDen = p;
+		this.firstNum = (n2.firstDen*n1.firstNum) + (n1.firstDen*n2.firstNum);
+		reduce();
+	}
+	public void subtract(Rational n1, Rational n2){
+		int p = n1.firstDen*n2.firstDen;
+		this.firstDen = p;
+		this.firstNum = (n2.firstDen*n1.firstNum) - (n1.firstDen*n2.firstNum);
+		reduce();
+	}
 	private void getGCF(int n1,int n2)
 	{
 		int rem = 0;
@@ -75,5 +121,3 @@ class Rational
 
 
 }
-
-
